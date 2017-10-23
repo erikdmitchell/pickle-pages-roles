07ae5060908859bd510822c11950821954ddb0a4
Erik <erikdmitchell@gmail.com>
2017-10-23 16:27:17 -0400

    limited specific post in admin section

--- admin/classes/posts-filters.php
+++ admin/classes/posts-filters.php
@@ -4,7 +4,8 @@ class Pickle_Pages_Roles_Admin_Posts_Filters {
 	
 	public function __construct() {
 		if (is_admin()) :
-			add_action('pre_get_posts', array($this, 'filter_posts_list'));
+			add_action('pre_get_posts', array($this, 'filter_posts_list')); 
+			add_action('admin_head', array($this, 'check_post_edit_access'));
 
 			if (!function_exists('wp_get_current_user'))
 				include(ABSPATH.'wp-includes/pluggable.php');
@@ -16,9 +17,9 @@ class Pickle_Pages_Roles_Admin_Posts_Filters {
 	public function filter_posts_list($query) {
 		$meta_arrays=array();
 		
-		//if (!$query->is_main_query())
-			//return;
-echo "filter posts list";		
+		if (!$query->is_main_query())
+			return;
+
 		// build "meta_keys" array //
 		foreach ($this->user->roles as $role) :
 			$meta_arrays[]=array(
@@ -34,6 +35,18 @@ echo "filter posts list";
 		);
 
 		$query->set('meta_query', $meta_query);
+	}
+	
+	public function check_post_edit_access() {
+		global $post;
+
+		foreach ($this->user->roles as $role) :
+			if (!ppr_can_role_edit($post->ID, $role)) :
+				wp_die(__('You do not have access to this post.'));
+			endif;
+		endforeach;
+			
+		return;
 	}
 	
 }
